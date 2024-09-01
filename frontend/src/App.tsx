762eeee0ecc14dd19ddddcbb195e95a8b70e46e8
manish <manish198646@yahoo.com>
2024-09-01 13:37:07 +0530

    make changes in the pages for mui and react-icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import Header from './components/Header'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Chat from './pages/Chat'
+import NotFound from './pages/NotFound'
 
 function App() {
 
   return (
     <main>
-      Hello World
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </main>
   )
 }
